712b487cf5d7f7b446db203162cdfd17b8f81a6c
KingHawkins <Kinghawkins61@gmail.com>
2023-07-06 20:33:26 +0300

    change

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """
-7-to_kv.py
+7-to_kv.
 """
 from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
-    Implementation
+    Implementation.
     """
     return (k, v * v)
